4e75f88154b063baa47cb2c9b4133de296875271
Kadir Sert <kadir.sert@pm.me>
2026-06-03 08:33:02 +0200

    misc: add rbql init script and zshrc backup

---
+++ .rbql_init_source.js
@@ -0,0 +1,12 @@
+// This file can be used to store RBQL UDFs. Example:
+//
+// function foo(value) {
+//     return 'foo ' + String(value.length);
+// }
+//
+// Functions defined in this file can be used in RBQL queries e.g.
+// SELECT foo(a1), a2 WHERE foo(a3) != 'foo 5' LIMIT 10
+//
+// Don't forget to save this file after editing!
+//
+// Write your own functions below this line:
